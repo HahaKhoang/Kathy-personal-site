50ade910aad85277d06941f62f230c53ca61c6fe
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-29 11:54:59 -0600

    Creates layout for TextContainer

--- src/TextContainer.jsx
+++ src/TextContainer.jsx
@@ -3,7 +3,19 @@ import styles from "./TextContainer.module.scss";
 function TextContainer() {
   return (
     <div className={styles.container}>
-      <p>Kathy</p>
+      <div className={styles["top-container"]}>
+        <p>Kathy</p>
+      </div>
+      <div className={styles["center-container"]}>
+        <h1>Creative. Frontend Developer.</h1>
+        <p>The portfolio page of Kathy Hoang</p>
+      </div>
+      <div className={styles["contact-container"]}>
+        <p>linkedin</p>
+        <p>linkedin</p>
+        <p>linkedin</p>
+        <p>linkedin</p>
+      </div>
     </div>
   );
 }
