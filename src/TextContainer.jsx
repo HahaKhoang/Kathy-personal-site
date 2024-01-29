b8983da27a860eca9f4606eb1d7135ac11e70d3a
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-29 11:42:09 -0600

    Creates layout for home page

---
+++ src/TextContainer.jsx
@@ -0,0 +1,11 @@
+import styles from "./TextContainer.module.scss";
+
+function TextContainer() {
+  return (
+    <div className={styles.container}>
+      <p>Kathy</p>
+    </div>
+  );
+}
+
+export default TextContainer;
