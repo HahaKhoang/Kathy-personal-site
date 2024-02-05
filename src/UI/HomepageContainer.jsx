af0561948f67aa20c9cdfb3bcbe6dfebc34320c9
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-05 14:06:29 -0600

    Sets up reaction-intersection-observer in HomepageContainer

--- src/UI/HomepageContainer.jsx
+++ src/UI/HomepageContainer.jsx
@@ -5,51 +5,76 @@ import auroraIcon from "../../public/img/auroraIcon.jpg";
 import bi from "../../public/img/bi.jpg";
 import jk from "../../public/img/jk.jpg";
 import jungwon from "../../public/img/jungwon.jpg";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
+
 import { NavLink } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import { useCallback, useRef } from "react";
 
 function HomepageContainer() {
-  //   gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef();
+  const intro = "Creative. Frontend Developer.";
 
-  //   useEffect(() => {
-  //     ScrollTrigger.create({
-  //       start: 0,
-  //       end: "max",
-  //       onLeave: (self) => {
-  //         self.scroll(1);
-  //         ScrollTrigger.update();
-  //       },
-  //       onLeaveBack: (self) => {
-  //         self.scroll(ScrollTrigger.maxScroll(window) - 1);
-  //         ScrollTrigger.update();
-  //       },
-  //     });
-  //   }, []);
+  const { ref: climateRef, inView: climateVisible } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: tabiRef, inView: tabiVisible } = useInView({ threshold: 0.9 });
+  const { ref: auroraRef, inView: auroraVisible } = useInView({
+    threshold: 0.9,
+  });
+
+  // const setRefs = useCallback(
+  //   (node) => {
+  //     ref.current = node;
+  //     climateRef(node);
+  //     tabiRef(node);
+  //     auroraRef(node);
+  //   },
+  //   [climateRef, tabiRef, auroraRef]
+  // );
+
+  const projects = [
+    {
+      src: cdwIcon,
+      ref: climateRef,
+      link: "/climate-death-wheel",
+      title: "Climate Death Wheel",
+    },
+    {
+      src: cha,
+      ref: tabiRef,
+      link: "/tabi-tomodachi",
+      title: "Tabi Tomodachi",
+    },
+    {
+      src: auroraIcon,
+      ref: auroraRef,
+      link: "/auroramalet",
+      title: "Auroramålet",
+    },
+  ];
 
   return (
     <div className={styles.container}>
       <div className={styles["text-container"]}>
         <div className={styles["center-container"]}>
-          <h1 className={styles.title}>Creative. Frontend Developer.</h1>
+          <h1 className={styles.title}>
+            {climateVisible && <p>{projects[0].title}</p>}
+            {tabiVisible && <p>{projects[1].title}</p>}
+            {auroraVisible && <p>{projects[2].title}</p>}
+          </h1>
           <p>The portfolio page of Kathy Hoang</p>
         </div>
       </div>
       <div className={styles["project-container"]}>
-        <NavLink to="/climate-death-wheel">
-          <img className={styles.project} src={cdwIcon} />
-        </NavLink>
-        <NavLink to="/tabi-tomodachi">
-          <img className={styles.project} src={cha} />
-        </NavLink>
-        <NavLink to="/auroramalet">
-          <img className={styles.project} src={auroraIcon} />
-        </NavLink>
-
-        <img className={styles.project} src={jungwon} />
-        <img className={styles.project} src={bi} />
-        <img className={styles.project} src={jk} />
+        {projects.map((project) => (
+          <NavLink to={project.link} key={project.title}>
+            <img
+              src={project.src}
+              className={styles.project}
+              ref={project.ref}
+            />
+          </NavLink>
+        ))}
       </div>
     </div>
   );
