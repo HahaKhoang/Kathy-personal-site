05e7d158ba9bc0da5996185e23607104751a2509
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-06 12:16:36 -0600

    Stylizes HomepageContainer

--- src/UI/HomepageContainer.jsx
+++ src/UI/HomepageContainer.jsx
@@ -29,24 +29,36 @@ function HomepageContainer() {
       ref: climateRef,
       link: "/climate-death-wheel",
       title: "Climate Death Wheel",
+      desc: "Interactive web component",
+      desc2: "Climate Climate Climate Climate ",
+      desc3: "Climate Climate Climate Climate Climate Climate Climate ",
     },
     {
       src: cha,
       ref: tabiRef,
       link: "/tabi-tomodachi",
       title: "Tabi Tomodachi",
+      desc: "Travel Agency Website ",
+      desc2: "Travel Travel Travel Travel Travel Travel ",
+      desc3: "Travel Travel Travel Travel Travel Travel Travel Travel Travel ",
     },
     {
       src: auroraIcon,
       ref: auroraRef,
       link: "/auroramalet",
       title: "Auroramålet",
+      desc: "Suing the Swedish State",
+      desc2: "Aurora Aurora Aurora Aurora Aurora ",
+      desc3: "Aurora Aurora Aurora Aurora Aurora Aurora Aurora Aurora ",
     },
     {
       src: bi,
       ref: oasisRef,
       link: "/the-wild-oasis",
       title: "The Wild Oasis",
+      desc: "Hotel booking site",
+      desc2: "Oasis Oasis Oasis Oasis ",
+      desc3: "Oasis Oasis Oasis Oasis Oasis Oasis Oasis Oasis Oasis Oasis ",
     },
   ];
 
@@ -60,8 +72,24 @@ function HomepageContainer() {
             {auroraVisible && projects[2].title}
             {oasisVisible && projects[3].title}
           </h1>
-
-          <p>The portfolio page of Kathy Hoang</p>
+          <p className={styles.desc}>
+            {climateVisible && projects[0].desc}
+            {tabiVisible && projects[1].desc}
+            {auroraVisible && projects[2].desc}
+            {oasisVisible && projects[3].desc}
+          </p>
+          <p className={styles.desc}>
+            {climateVisible && projects[0].desc2}
+            {tabiVisible && projects[1].desc2}
+            {auroraVisible && projects[2].desc2}
+            {oasisVisible && projects[3].desc2}
+          </p>
+          <p className={styles.desc}>
+            {climateVisible && projects[0].desc3}
+            {tabiVisible && projects[1].desc3}
+            {auroraVisible && projects[2].desc3}
+            {oasisVisible && projects[3].desc3}
+          </p>
         </div>
       </div>
       <div className={styles["project-container"]}>
