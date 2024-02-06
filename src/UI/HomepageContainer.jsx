7929691d03c7f677517c363d0b0eba8fc8d581b5
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-06 11:47:19 -0600

    Sets text to change depending on scroll behavior in HomepageContainer

--- src/UI/HomepageContainer.jsx
+++ src/UI/HomepageContainer.jsx
@@ -8,29 +8,20 @@ import jungwon from "../../public/img/jungwon.jpg";
 
 import { NavLink } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
-import { useCallback, useRef } from "react";
 
 function HomepageContainer() {
-  const ref = useRef();
-  const intro = "Creative. Frontend Developer.";
-
   const { ref: climateRef, inView: climateVisible } = useInView({
-    threshold: 0.9,
+    threshold: 0.6,
   });
-  const { ref: tabiRef, inView: tabiVisible } = useInView({ threshold: 0.9 });
+  const { ref: tabiRef, inView: tabiVisible } = useInView({
+    threshold: 0.6,
+  });
   const { ref: auroraRef, inView: auroraVisible } = useInView({
-    threshold: 0.9,
+    threshold: 0.6,
   });
-
-  // const setRefs = useCallback(
-  //   (node) => {
-  //     ref.current = node;
-  //     climateRef(node);
-  //     tabiRef(node);
-  //     auroraRef(node);
-  //   },
-  //   [climateRef, tabiRef, auroraRef]
-  // );
+  const { ref: oasisRef, inView: oasisVisible } = useInView({
+    threshold: 0.6,
+  });
 
   const projects = [
     {
@@ -51,6 +42,12 @@ function HomepageContainer() {
       link: "/auroramalet",
       title: "Auroramålet",
     },
+    {
+      src: bi,
+      ref: oasisRef,
+      link: "/the-wild-oasis",
+      title: "The Wild Oasis",
+    },
   ];
 
   return (
@@ -58,10 +55,12 @@ function HomepageContainer() {
       <div className={styles["text-container"]}>
         <div className={styles["center-container"]}>
           <h1 className={styles.title}>
-            {climateVisible && <p>{projects[0].title}</p>}
-            {tabiVisible && <p>{projects[1].title}</p>}
-            {auroraVisible && <p>{projects[2].title}</p>}
+            {climateVisible && projects[0].title}
+            {tabiVisible && projects[1].title}
+            {auroraVisible && projects[2].title}
+            {oasisVisible && projects[3].title}
           </h1>
+
           <p>The portfolio page of Kathy Hoang</p>
         </div>
       </div>
